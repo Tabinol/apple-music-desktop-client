da48b1cf207f91bdc8d770542150bed2eaca2553
Tabinol <8842894+Tabinol@users.noreply.github.com>
2024-01-03 15:31:55 -0500

    Fix flatpak-dist.sh

--- flatpak/flatpak-dist.sh
+++ flatpak/flatpak-dist.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-PACKAGE_VERSION=`node -p "require('./package.json').version"`
-
 cd "`dirname "$0"`"
 
+PACKAGE_VERSION=`jq --raw-output '.version' ../package.json`
+
+flatpak-node-generator yarn ../yarn.lock
 flatpak-builder build me.tabinol.apple-music-desktop-client.yml --repo=repo --force-clean
 mkdir -p ../dist
 flatpak build-bundle repo ../dist/apple-music-desktop-client-${PACKAGE_VERSION}.flatpak me.tabinol.apple-music-desktop-client
